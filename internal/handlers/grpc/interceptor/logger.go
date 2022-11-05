6a5eeb68bfdff9d3206bdef0c89970dba1e21ce5
Sergey Kamenskiy <skvirp@gmail.com>
2022-11-05 12:02:02 +0100

    Add client IP to logger message

--- internal/handlers/grpc/interceptor/logger.go
+++ internal/handlers/grpc/interceptor/logger.go
@@ -5,13 +5,18 @@ import (
 	"log"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/peer"
 )
 
 func Logger() grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
-		log.Printf("request started, method: %s", info.FullMethod)
+		var clientIP string
+		if p, ok := peer.FromContext(ctx); ok {
+			clientIP = p.Addr.String()
+		}
+		log.Printf("request started, method: %s, client: %s", info.FullMethod, clientIP)
 		h, err := handler(ctx, req)
-		log.Printf("request completed, method: %s", info.FullMethod)
+		log.Printf("request completed, method: %s, client: %s", info.FullMethod, clientIP)
 		return h, err
 	}
 }
