cad5231d0a4419aaa4d3fb6029272b91e7612f51
Sergey Kamenskiy <skvirp@gmail.com>
2022-11-15 10:33:32 +0500

    Add config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net"
 	"os"
@@ -9,14 +10,37 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/ardanlabs/conf/v3"
 	"go.uber.org/zap"
 
 	"keeper/internal/handlers/server"
 	"keeper/internal/repository/cloud"
+	"keeper/internal/repository/memory"
 	"keeper/internal/services"
 	s3 "keeper/pkg/cloud"
 	"keeper/pkg/logger"
 )
+
+var build = "develop"
+
+const (
+	passwordHashCost = 13
+)
+
+type config struct {
+	conf.Version
+	Address         string        `conf:"default:0.0.0.0:3200"`
+	ShutdownTimeout time.Duration `conf:"default:5s"`
+	TokenLifetime   time.Duration `conf:"default:24h"`
+	StorageType     string        `conf:"default:memory,help:Storage type can be memory or cloud"`
+	CloudStorage    struct {
+		Bucket   string `conf:"default:keeper"`
+		Endpoint string `conf:"default:http://localhost:4566"`
+		Region   string `conf:"default:us-east-1"`
+		Key      string `conf:"default:key"`
+		Secret   string `conf:"default:secret"`
+	}
+}
 
 func main() {
 	log, err := logger.New("KEEPER-SERVER")
@@ -36,20 +60,104 @@ func main() {
 }
 
 func run(log *zap.SugaredLogger) error {
-	listen, err := net.Listen("tcp", ":3200")
+
+	// =========================================================================
+	// Configuration
+
+	cfg := config{
+		Version: conf.Version{
+			Build: build,
+			Desc:  "GophKeeper GRPC Server",
+		},
+	}
+	const prefix = "KEEPER"
+	help, err := conf.Parse(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parse config: %w", err)
+	}
+
+	// =========================================================================
+	// Create GRPC Server
+
+	serverError := make(chan error, 1)
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	s, err := buildServer(log, cfg)
+	if err != nil {
+		return fmt.Errorf("build server: %w", err)
+	}
+
+	// =========================================================================
+	// App Starting
+
+	log.Infow("starting service", "version", build)
+
+	listen, err := net.Listen("tcp", cfg.Address)
 	if err != nil {
 		panic(err)
 	}
 
+	go func() {
+		log.Infow("startup", "status", "grpc server started", "host", listen.Addr())
+		serverError <- s.Serve(listen)
+	}()
+
+	// =========================================================================
+	// Shutdown
+
+	select {
+	case err := <-serverError:
+		return fmt.Errorf("server error: %w", err)
+	case sig := <-shutdown:
+		log.Infow("shutdown", "status", "shutdown started", "signal", sig)
+		defer log.Infow("shutdown", "status", "shutdown complete", "signal", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), cfg.ShutdownTimeout)
+		defer cancel()
+
+		if err := s.Shutdown(ctx); err != nil {
+			s.Close()
+			return fmt.Errorf("could not stop grpc server gracefully: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func buildServer(log *zap.SugaredLogger, cfg config) (*server.KeeperServer, error) {
 	idGenerator := services.UuidGenerator{}
 	passwordHasher := services.BCryptPasswordHasher{
-		Cost: 13,
+		Cost: passwordHashCost,
 	}
-	//userRepository := memory.NewUserRepository()
-	s3Client := s3.NewS3Client("key", "secret", "us-east-1", "http://localhost:4566")
-	userRepository := cloud.NewUserRepository(s3Client, "keeper")
-	//tokenRepository := memory.NewTokenRepository(24 * time.Hour)
-	tokenRepository := cloud.NewTokenRepository(s3Client, "keeper", 1*time.Hour)
+
+	var userRepository services.UserRepository
+	var tokenRepository services.TokenRepository
+	var itemRepository services.ItemRepository
+
+	switch cfg.StorageType {
+	case "memory":
+		userRepository = memory.NewUserRepository()
+		tokenRepository = memory.NewTokenRepository(cfg.TokenLifetime)
+		itemRepository = memory.NewItemRepository()
+	case "cloud":
+		s3Client := s3.NewS3Client(
+			cfg.CloudStorage.Key,
+			cfg.CloudStorage.Secret,
+			cfg.CloudStorage.Region,
+			cfg.CloudStorage.Endpoint,
+		)
+		userRepository = cloud.NewUserRepository(s3Client, cfg.CloudStorage.Bucket)
+		tokenRepository = cloud.NewTokenRepository(s3Client, cfg.CloudStorage.Bucket, cfg.TokenLifetime)
+		itemRepository = cloud.NewItemRepository(s3Client, cfg.CloudStorage.Bucket)
+	default:
+		return nil, fmt.Errorf("not supported storage type: %s", cfg.StorageType)
+	}
+
 	authService := services.NewAuthService(
 		&idGenerator,
 		&passwordHasher,
@@ -60,8 +168,6 @@ func run(log *zap.SugaredLogger) error {
 		userRepository,
 		tokenRepository,
 	)
-	//itemRepository := memory.NewItemRepository()
-	itemRepository := cloud.NewItemRepository(s3Client, "keeper")
 	itemService := services.NewItemService(&idGenerator, itemRepository)
 
 	s := server.NewKeeperServer(server.KeeperServerConfig{
@@ -71,30 +177,5 @@ func run(log *zap.SugaredLogger) error {
 		Item:  itemService,
 	})
 
-	serverError := make(chan error, 1)
-	shutdown := make(chan os.Signal, 1)
-	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
-
-	go func() {
-		log.Infow("startup", "status", "grpc server started", "host", listen.Addr())
-		serverError <- s.Serve(listen)
-	}()
-
-	select {
-	case err := <-serverError:
-		return fmt.Errorf("server error: %w", err)
-	case sig := <-shutdown:
-		log.Infow("shutdown", "status", "shutdown started", "signal", sig)
-		defer log.Infow("shutdown", "status", "shutdown complete", "signal", sig)
-
-		ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
-		defer cancel()
-
-		if err := s.Shutdown(ctx); err != nil {
-			s.Close()
-			return fmt.Errorf("could not stop grpc server gracefully: %w", err)
-		}
-	}
-
-	return nil
+	return s, nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module keeper
 go 1.19
 
 require (
+	github.com/ardanlabs/conf/v3 v3.1.2
+	github.com/aws/aws-sdk-go-v2 v1.17.1
+	github.com/aws/aws-sdk-go-v2/credentials v1.13.0
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.29.2
 	github.com/google/uuid v1.3.0
 	go.uber.org/zap v1.23.0
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
@@ -11,23 +15,14 @@ require (
 )
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.17.1 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.9 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.18.0 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.13.0 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.19 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.25 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.19 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.26 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.16 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.10 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.20 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.19 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.13.19 // indirect
-	github.com/aws/aws-sdk-go-v2/service/s3 v1.29.2 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sso v1.11.25 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.13.8 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.17.2 // indirect
 	github.com/aws/smithy-go v1.13.4 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
